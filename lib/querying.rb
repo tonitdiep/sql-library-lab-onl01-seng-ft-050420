58620ddf400a7b5eaad156078a0ded41322e86fa
Toni  Diep <adiep123@gmail.com>
2020-06-11 00:33:05 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,31 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id GROUP by series ORDER BY books.year"
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year";
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH (characters.motto) DESC LIMIT 1";
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY characters.species ORDER BY characters.species DESC LIMIT 1";
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM series 
+  INNER JOIN authors 
+  ON authors.id = series.author_id
+  INNER JOIN subgenres
+  ON subgenres.id = series.subgenre_id";
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM characters 
+  INNER JOIN series 
+  ON characters.id = series.id
+  GROUP BY series.title, characters.species
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(characters.species) DESC LIMIT 1";
 end
 
 def select_character_names_and_number_of_books_they_are_in

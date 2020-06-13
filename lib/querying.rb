07699477de8e92113f38259fe0ae1fd3c809f3bf
Toni  Diep <adiep123@gmail.com>
2020-06-13 22:23:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,10 +39,10 @@ def select_character_names_and_number_of_books_they_are_in
    GROUP BY characters.name
    ORDER BY COUNT(characters.name) DESC";
   
-  # "SELECT characters.name, COUNT (name)
-  # FROM character_books 
-  # INNER JOIN characters
-  # ON characters.id = character_books.character_id
-  # GROUP BY characters.name
-  # ORDER BY COUNT(name) DESC";
+  "SELECT characters.name, COUNT (name)
+  FROM character_books 
+  INNER JOIN characters
+  ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(name) DESC";
 end
